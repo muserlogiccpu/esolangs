21fd5a0ab0e1fd235de679a00d9a0c521297fea3
muserlogiccpu <developmentmuser@gmail.com>
2026-03-30 16:54:35 +0300

    Create main.lua

---
+++ transpilers/deadfish/lua/main.lua
@@ -0,0 +1,16 @@
+if #arg ~= 2:
+    error("expected 1 argument, not " .. #arg .. ", usage: " .. arg[0] .. " <file>")
+print("local acc = 0")
+for i = 1, #arg[1] do
+    local chr = string.sub(arg[1], i, i)
+    if chr == "i" then
+        print("local acc = acc + 1")
+    elseif chr == "d" then
+        print("local acc = acc - 1")
+    elseif chr == "s" then
+        print("local acc = acc ^ 2")
+    elseif chr == "o" then
+        print("io.write(tostring(acc))")
+    end
+    print("if acc == 256 or acc == -1 then acc = 0 end")
+end
